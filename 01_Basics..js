e4a05c87a685726b409fff522522ab01af0452e6
Satyam-Viz <satyam963670@gmail.com>
2026-01-23 09:07:31 +0000

    Written All the Basic datatype and how to find the type of any declared variable

---
+++ 01_Basics..js
@@ -0,0 +1,20 @@
+//First Javascript File in which we talk about 
+
+//Declaration of constant
+const accountId = 1234
+
+//Declaration of Variable
+var accountName = "Satyam Vishwakarma"
+let accountPassword = "56sa65"
+let account = true
+
+let n = null
+let m;
+//Output of Declaration
+console.log(accountId, accountName, accountPassword, n, m)
+
+//Prints a tabular for of Declaration
+console.table([accountId, accountName, accountPassword, account])
+
+//Prints a table in which Datatype for is Written
+console.table([typeof(accountId), typeof(accountName), typeof(accountPassword), typeof(account), typeof(n), typeof(m)])
