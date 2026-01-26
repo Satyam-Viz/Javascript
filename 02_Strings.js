a70c0262f35a01c07e6abad6eaae2ce66883a585
Satyam-Viz <satyam963670@gmail.com>
2026-01-26 16:03:18 +0000

    Added file of Complete Strings and its sub-types

---
+++ 02_Strings.js
@@ -0,0 +1,36 @@
+let userName = "Satyam Vishwakarma"
+
+const game = new String("Satyam Vishwakarma")
+console.log(game)
+
+//For find the Character at with the index
+console.log(game.charAt(0))
+console.log(game.charAt(1))
+console.log(game.charAt(2))
+console.log(game.charAt(3))
+console.log(game.charAt(4))
+console.log(game.charAt(5))
+console.log(game.charAt(6))
+console.log(game.charAt(7))
+console.log(game.charAt(8))
+console.log(game.charAt(9))
+console.log(game.charAt(10))
+console.log(game.charAt(11))
+console.log(game.charAt(12))
+console.log(game.charAt(13))
+
+//To Convert the String in Uppercase and Lowercase
+console.log(game.toUpperCase())
+console.log(game.toLowerCase())
+
+//To split also (in this enter the split by)
+console.log(game.split[" "])
+
+//Length of String
+console.log(game.length)
+console.log(game.bold())
+
+
+
+
+
