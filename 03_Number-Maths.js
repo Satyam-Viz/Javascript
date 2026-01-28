5984c9678821394252603a6428776303330b8aaa
Satyam-Viz <satyam963670@gmail.com>
2026-01-28 12:55:38 +0000

    Added the file of Number and Maths in JavaScripts

---
+++ 03_Number-Maths.js
@@ -0,0 +1,18 @@
+//Right wat to represent a Number in a targeted way
+const score = new Number("100")
+console.log(score)
+
+//Similar way of represneting it
+const balance = 101
+console.log(balance)
+
+//how it is differs
+console.log(typeof score)
+console.log(typeof balance)
+
+const bal1 = 23.55555
+console.log(bal1.toPrecision(3))
+console.log(Math.round(bal1))
+console.log(Math.ceil(bal1))
+console.log(Math.floor(bal1))
+console.log(Math.abs(-987))
